a222454f26c67bec413f6ed58c1bcc640d9ca506
Colin Pitrat <colin.pitrat@gmail.com>
2018-10-03 11:24:47 +0100

    Include both memory and swap in i3status

--- bin/my_i3status.sh
+++ bin/my_i3status.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+
+function make_stanza() {
+  stanza_name=$1
+  stanza_value=$2
+  echo '{"name":"'$stanza_name'","color":"#FFFFFF","markup":"none","full_text":"'$stanza_value'"}'
+}
+
 i3status | while :
 do
   read line
+  mem_usage=`echo 100-100*$(grep MemFree /proc/meminfo | awk '{ print $2 }')/$(grep MemTotal /proc/meminfo | awk '{ print $2 }') | bc`
+  mem_stanza=`make_stanza "mem", "M: $mem_usage%"`
   swap_usage=`echo 100-100*$(grep SwapFree /proc/meminfo | awk '{ print $2 }')/$(grep SwapTotal /proc/meminfo | awk '{ print $2 }') | bc`
-  swap_stanza='{"name":"swap","color":"#FFFFFF","markup":"none","full_text":"'$swap_usage'%"}'
+  swap_stanza=`make_stanza "swap", "S: $swap_usage%"`
   echo $line | \
     sed 's/\("full_text":"MTV: \)/"color":"#FFFF00",\1/' | \
     sed 's/\("full_text":"DUB: \)/"color":"#8080FF",\1/' | \
-    sed "s/\[{/\[${swap_stanza}{/"
+    sed "s/\[{/\[${mem_stanza},${swap_stanza},{/"
 done
