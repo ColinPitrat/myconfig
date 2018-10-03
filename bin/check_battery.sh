fbf7ce05aaf659893d9fbe5fb06c343db7ec033a
Colin Pitrat <colin.pitrat@gmail.com>
2018-10-03 11:17:52 +0100

    Improve check for battery

--- bin/check_battery.sh
+++ bin/check_battery.sh
@@ -9,9 +9,11 @@ if [ $charge -lt 15 -a "$status" != "Charging" ]
 then
   export DISPLAY=:0.0
   zenity --warning --title "Fais gaffe !" --text "Bientôt plus de batterie mec !" &
-  mplayer /usr/share/sounds/KDE-Sys-App-Error-Serious-Very.ogg
-  sleep 5
-  mplayer /usr/share/sounds/KDE-Sys-App-Error-Serious-Very.ogg
-  sleep 5
-  mplayer /usr/share/sounds/KDE-Sys-App-Error-Serious-Very.ogg
+  # Repeat multiple times just in case I miss it
+  for i in `seq 1 3`
+  do
+    # Play multiple sounds as the presence of the files depend on the computer ...
+    mplayer /usr/share/sounds/KDE-Sys-App-Error-Serious-Very.ogg /usr/share/sounds/ubuntu/stereo/message-new-instant.ogg
+    sleep 5
+  done
 fi
