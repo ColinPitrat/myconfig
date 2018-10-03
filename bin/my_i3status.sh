0a860b3570319ab60bbc1ec91666ef7a8e994a65
Colin Pitrat <colin.pitrat@gmail.com>
2018-10-03 11:11:30 +0100

    Add swap information

--- bin/my_i3status.sh
+++ bin/my_i3status.sh
@@ -2,7 +2,10 @@
 i3status | while :
 do
   read line
+  swap_usage=`echo 100-100*$(grep SwapFree /proc/meminfo | awk '{ print $2 }')/$(grep SwapTotal /proc/meminfo | awk '{ print $2 }') | bc`
+  swap_stanza='{"name":"swap","color":"#FFFFFF","markup":"none","full_text":"'$swap_usage'%"}'
   echo $line | \
     sed 's/\("full_text":"MTV: \)/"color":"#FFFF00",\1/' | \
-    sed 's/\("full_text":"DUB: \)/"color":"#8080FF",\1/'
+    sed 's/\("full_text":"DUB: \)/"color":"#8080FF",\1/' | \
+    sed "s/\[{/\[${swap_stanza}{/"
 done
